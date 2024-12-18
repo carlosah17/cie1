06a81c56d5b9204ba4d10c86d70e463c8139edd3
cecilioAlvarez <cmac@arquitecturajava.com>
2024-12-18 11:34:38 +0100

    nuevo

--- java03/src/es/curso8/Principal10.java
+++ java03/src/es/curso8/Principal10.java
@@ -9,30 +9,27 @@ import java.sql.SQLException;
 public class Principal10 {
 
 	public static void main(String[] args) {
-
-		PersonaRepositoryOk repo = new PersonaRepositoryOk();
-		Persona p = new Persona("pepe");
-		// el metodo nuestro de buscarTodos
-			// los datos
-		ResultSet rs = repo.buscarTodos();
-
 		
-		generaficheropersonas(rs);
+		PersonaRepositoryOk repo= 
+				new PersonaRepositoryOk();
 
-	}
-
-	private static void generaficheropersonas(ResultSet rs) {
-		try (FileWriter fstream = new FileWriter("personas.txt"); BufferedWriter info = new BufferedWriter(fstream)) {
-
-			while (rs.next()) {
-				info.write(rs.getString("nombre"));
-				info.write(rs.getString("apellidos"));
-				info.write(rs.getInt("edad"));
+		ResultSet rs=repo.buscarTodos();
+		
+		try {
+			while(rs.next()) {
+				
+				System.out.println(rs.getString("nombre"));
+				System.out.println(rs.getString("apellidos"));
+				System.out.println(rs.getInt("edad"));
+				
 			}
-
-		} catch (Exception e) {
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+	
 	}
 
 }
